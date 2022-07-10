ec90a2fdb25666fc27a599be085a61c1c48d1dc4
OverEuro <yangyijun94@163.com>
2022-07-10 18:20:03 +1000

    ewc

--- run_experiments.sh
+++ run_experiments.sh
@@ -17,13 +17,23 @@ seed="0" # seeds = {0, 2, 4, 6, 8}
 # cl_task >$path 2>&1 &
 
 # For L2
-path="tmp_cl_l2_"$seed".output"
+# path="tmp_cl_l2_"$seed".output"
+
+# nohup python run_cl.py \
+# --tasks=CW10 \
+# --seed=$seed \
+# --cl_method=l2 \
+# --cl_reg_coef=100000.0 \
+# --logger_output tsv \
+# --group_id cl_task >$path 2>&1 &
+
+# For EWC
+path="tmp_cl_ewc_"$seed".output"
 
 nohup python run_cl.py \
 --tasks=CW10 \
---seed=0 \
---cl_method=l2 \
---cl_reg_coef=100000.0 \
+--seed=$seed \
+--cl_method=ewc \
+--cl_reg_coef=10000.0 \
 --logger_output tsv \
 --group_id cl_task >$path 2>&1 &
-
